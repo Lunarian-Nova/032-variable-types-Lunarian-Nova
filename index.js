98d12a5f5cc8f945457f5692547f27f440faab60
Lunarian-Nova <ckeister@anamosa.k12.ia.us>
2026-03-06 18:14:45 +0000

    lmao

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+let animalName = "Cat";  // string variable
+let height = 25;         // numeric variable
+let isIndoorPet = true;  // boolean variable
 
+document.getElementById('result').innerHTML = animalName;
