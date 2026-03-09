cb76d317cab908165ff300e27d8942eb99be3420
Lunarian-Nova <ckeister@anamosa.k12.ia.us>
2026-03-09 16:58:59 +0000

    lmao

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ let height = 25;         // numeric variable
 let isIndoorPet = true;  // boolean variable
 
 document.getElementById('result').innerHTML = animalName;
+document.getElementById('height-display').innerHTML = "Height: " + height;
+document.getElementById('indoor-display').innerHTML = "Is Indoor Pet: " + isIndoorPet;
